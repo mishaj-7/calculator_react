eb69b7d10ec1ba3009b2ba8d876b270a9ff1c58a
Mishaj__k <amstig100@gmail.com>
2023-12-21 12:33:28 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1 style={{ color: 'red', paddingLeft: '100px', fontStyle: 'italic' }}>Calculator</h1>
+      <div className='calculator'>
+
+        <div className='display'>
+
+
+        </div>
+
+        div
+
+      </div>
+    </>
+  );
+}
+
+export default App
